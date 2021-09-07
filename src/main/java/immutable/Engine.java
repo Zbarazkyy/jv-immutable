51f9b41b132f4f483e84c81d1e6643b8c8a2a269
P.Zbarazkyy <mtu09@ukr.net>
2021-09-07 13:21:28 +0300

    Initial commit

---
+++ src/main/java/immutable/Engine.java
@@ -0,0 +1,59 @@
+package immutable;
+
+import java.util.Objects;
+
+public class Engine  implements Cloneable {
+    private int horsePower;
+    private String manufacturer;
+
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public Engine clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Сan't clone!!!", e);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Engine engine = (Engine) o;
+        return horsePower == engine.horsePower && manufacturer.equals(engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{"
+                + "horsePower=" + horsePower
+                + ", manufacturer='" + manufacturer + '\''
+                + '}';
+    }
+}
